bcd7913d03499f87592e2917319fc5f92ca42e67
fuljencio <fuljencio@0346d35e-e29e-11de-9e61-43d41527e390>
2009-12-20 16:20:08 +0000

    removed: 10 extra points on rules.

--- mastermind/src/UseCaseController/ShowRulesUseCaseController.java
+++ mastermind/src/UseCaseController/ShowRulesUseCaseController.java
@@ -11,7 +11,8 @@ public class ShowRulesUseCaseController extends GenericShowRulesUCC {
         Rules = "The game is played using:\n" +
                 "   * A decoding board, with a shield at one end covering a row of four large holes, and ten additional rows containing four large holes next to a set of four small holes;\n" +
                 "   * Code pegs of six different colors, with round heads, which will be placed in the large holes on the board; and \n" +
-                "   * key pegs, black and white, smaller than the code pegs; they will be placed in the small holes on the board. \n\n" +
+                "   * key pegs, black and white, smaller than the code pegs; they will be placed in the small holes on the board. \n" +
+                "\n" +
                 "Six rounds will be played. On each round, one player becomes the codemaker, the other the codebreaker, alternatively. \n" +
                 "1. CODEMAKER ACTION: The codemaker chooses a pattern. The number of code pegs and if the duplicates are allowed depends of the level, so the player could even choose four code pegs of the same color. The chosen pattern is placed in the four holes covered by the shield, visible to the codemaker but not to the codebreaker.\n" +
                 "2. CODEBREAKER : The codebreaker tries to guess the pattern, in both order and color, within ten turns. Each guess is made by placing a row of code pegs on the decoding board.\n" +
@@ -28,12 +29,12 @@ public class ShowRulesUseCaseController extends GenericShowRulesUCC {
                 "   * and a red peg for the red." +
                 "  No indication is given of the fact that the pattern also includes a second red.\n" +
                 "4. CODEBREAKER : Once feedback is provided, another guess is made; guesses and feedback continue to alternate until either the codebreaker guesses correctly, or ten incorrect guesses are made.\n" +
+                "\n" +
                 "SCORING\n" +
                 "Depending on the difficulty level of the game, the codemaker earns a variable amount of points for each guess a codebreaker makes\n" +
                 " · Easy level: 20 points\n" +
                 " · Medium level: 15 points\n" +
                 " · Hard level: 10 points\n" +
-                "The codemaker gets 10 extra points if the codebreaker doesn't guess the pattern exactly in the last guess.\n" +
                 "The winner is the one who has the most points after all six rounds are played.\n";
     }
 
